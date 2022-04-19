6b905e0cde4faccde646de636bd24318a740ad9b
Jason Cameron <jason@alley.co>
2022-04-18 23:10:13 -0400

    js docs added

--- utils/sort-by-date.js
+++ utils/sort-by-date.js
@@ -1,3 +1,10 @@
+/**
+ * Takes params A and B and sorts data in descending order.
+ * @param a - date
+ * @param b - date
+ * @returns data in descending order..
+ */
+
 const sortByDate = (a, b) => {
     return new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
 
